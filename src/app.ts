5cb7c40ff2b012160d8ecdddadeffbed2ce6d583
Austin Nieset <ajnieset@gmail.com>
2020-06-10 23:42:43 -0400

    added basic code to app.ts and fixed errors in tsconfig

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,10 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.json());
