d30cc6c3de234bc16f6618c11c8bdad6b59a02e4
Austin Nieset <ajnieset@gmail.com>
2020-06-10 23:51:35 -0400

    added listen to server

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
-import express from 'express';
+import express, { Application } from 'express';
 import path from 'path';
 
-const app = express();
+const app:Application = express();
+const port: number = 3000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`App is listing on ${port}`)
+});
