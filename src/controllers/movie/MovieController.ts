03df99141c1ddfdf93ed3a43e02987e9ef8caf21
Austin Nieset <ajnieset@gmail.com>
2020-06-14 20:55:02 -0400

    updated views and added new view to addMovie page

--- src/controllers/movie/MovieController.ts
+++ src/controllers/movie/MovieController.ts
@@ -4,7 +4,7 @@ import Movie from '../../models/movie';
 
 export class MovieController extends BaseController {
     public create(req: Request<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs>, res: Response<any>): void {
-        throw new Error("Method not implemented.");
+        res.render('createMovie');
     }
     public read(req: Request<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs>, res: Response<any>): void {
         Movie.find({}, (err, watchlist) => {res.render('movie', watchlist[0])});
