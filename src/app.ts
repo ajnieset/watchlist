df447f0dcf58bcf54ce7ed039cd51d325dcaf0fd
Austin Nieset <ajnieset@gmail.com>
2020-06-16 22:04:02 -0400

    moved db connect to .env file

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
+import dotenv from 'dotenv';
 import express, { Application, Request, Response, NextFunction } from 'express';
 import path from 'path';
 import mongoose, { Connection } from 'mongoose';
 
 import { movieRouter } from './routes/routes';
 
+dotenv.config();
+
 const app: Application = express();
 const port: number = 3000;
 
 //DB connection
-const connection: string = 'mongodb://127.0.0.1:27017/movie';
+const connection: string = process.env.DB_CONNECT ? process.env.DB_CONNECT : "";
 mongoose.connect(connection, { useNewUrlParser: true, useUnifiedTopology: true });
 const db: Connection = mongoose.connection;
 
